13155c580cf22d141d3ee9f3f5413f9d26d72584
ogusam <ogusam32@gmail.com>
2024-05-31 16:38:09 +0200

    first commit

--- script.js
+++ script.js
@@ -55,34 +55,36 @@ function myFunction() {
 
 
   
-  var slideIndex = 1;
-showDivs(slideIndex);
+  
+/*slider*/
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+let slider = tns({
+    container : ".my-slider",
+    "slideBy" : "1",
+    "speed" : 400,
+    "nav" : false,
+    autoplay : true,
+    controls: false,
+    autoplayButtonOutput :false,
+    responsive: {
+        1600:{
+            items : 4,
+            gutter : 20
+        },
+        1024: {
+            items:3,
+            gutter:20 
+        },
+        768: {
+            items: 3,
+            gutter: 20,
+        },
+        480:{
+            items: 1,
+            gutter:20,
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
+        }
+    }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
-
-
-
-/*form*/
+})
 
