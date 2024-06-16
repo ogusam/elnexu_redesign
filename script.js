90e60867d38cbd7e2cdd997f8b4d60900c98dd30
ogusam <ogusam32@gmail.com>
2024-06-16 19:26:00 +0200

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
-
-
 
 
 const nav =document.querySelector('.nav_links');
@@ -74,32 +58,6 @@ function myFunction() {
 
   
   
-
-const btns = document.querySelectorAll(".nav-btn");
-const slides = document.querySelectorAll(".img-slide");
-const contents = document.querySelectorAll(".content");
-
-var sliderNav = function(manual){
-  btns.forEach((btn)=>{
-    btn.classList.remove("active");
-  });
-  slides.forEach((slide)=>{
-    slide.classList.remove("active");
-  });
-
-  contents.forEach((content)=>{
-    content.classList.remove("active");
-  });
-
-  btns[manual].classList.add("active");
-  slides[manual].classList.add("active");
-  contents[manual].classList.add("active");
-};
-  btns.forEach((btn, i)=>{
-    btn.addEventListener("click", ()=>{
-      sliderNav(i);
-    });
-  });
 
 
 
